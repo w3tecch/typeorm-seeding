b9ad4008fe8816be7619b1159c1de0af8812ae28
hirsch <gery.hirschfeld@w3tec.ch>
2018-10-24 08:35:26 +0200

    :sparkles: Usage of dates within seeds #124

--- src/utils.ts
+++ src/utils.ts
@@ -6,16 +6,16 @@ export const getNameOfClass = (c: any): string => new c().constructor.name;
 /**
  * Checks if the given argument is a promise
  */
-export const isPromiseLike = (o: any): boolean => !!o && (typeof o === 'object' || typeof o === 'function') && typeof o.then === 'function';
-
+export const isPromiseLike = (o: any): boolean =>
+  !!o && (typeof o === 'object' || typeof o === 'function') && typeof o.then === 'function' && !(o instanceof Date);
 /**
  * Times repeats a function n times
  */
 export const times = async <TResult>(n: number, iteratee: (index: number) => Promise<TResult>): Promise<TResult[]> => {
-    const rs = [] as TResult[];
-    for (let i = 0; i < n; i++) {
-        const r = await iteratee(i);
-        rs.push(r);
-    }
-    return rs;
+  const rs = [] as TResult[];
+  for (let i = 0; i < n; i++) {
+    const r = await iteratee(i);
+    rs.push(r);
+  }
+  return rs;
 };
