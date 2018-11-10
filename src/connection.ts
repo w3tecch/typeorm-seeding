fd35b4b4132448469c01749cc273537344ddb326
hirsch <gery.hirschfeld@w3tec.ch>
2018-11-10 08:39:44 +0100

    :sparkles: Improve loading ormconfig file

---
+++ src/connection.ts
@@ -0,0 +1,38 @@
+import * as path from 'path';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
+
+const args = process.argv;
+
+// Get cli parameter for logging
+const logging = args.indexOf('--logging') >= 0 || args.indexOf('-L') >= 0 || false;
+
+// Get cli parameter for ormconfig.json or another json file
+const configParam = '--config';
+const hasConfigPath = args.indexOf(configParam) >= 0 || false;
+const indexOfConfigPath = args.indexOf(configParam) + 1;
+
+/**
+ * Returns a TypeORM database connection for our entity-manager
+ */
+export const loadConnection = async (): Promise<Connection> => {
+  let ormconfig: any = {
+    logging,
+  };
+
+  if (hasConfigPath) {
+    const configPath = path.join(process.cwd(), args[indexOfConfigPath]);
+    ormconfig = require(configPath);
+
+  } else {
+    try {
+      ormconfig = await getConnectionOptions();
+    } catch (e) {
+      ormconfig = require(path.join(process.cwd(), 'ormconfig.json'));
+    }
+
+  }
+
+  // ormconfig.entities = [];
+
+  return createConnection(ormconfig);
+};
