f1bbdbf8a7f4a761614a34b9b8cdea4460bb9e73
hirsch <gery.hirschfeld@w3tec.ch>
2019-06-04 22:16:40 +0200

    :sparkles: Add config cli command

---
+++ src/commands/config.command.ts
@@ -0,0 +1,32 @@
+import * as yargs from 'yargs'
+import chalk from 'chalk'
+import { getConnectionOptions } from '../typeorm-seeding'
+import * as pkg from '../../package.json'
+import { printError } from '../utils/log.util'
+
+export class ConfigCommand implements yargs.CommandModule {
+  command = 'config'
+  describe = 'Show the TypeORM config'
+
+  builder(args: yargs.Argv) {
+    return args
+      .option('c', {
+        alias: 'config',
+        default: 'ormconfig.js',
+        describe: 'Path to the typeorm config file (json or js).',
+      })
+  }
+
+  async handler(args: yargs.Arguments) {
+    const log = console.log
+    log(chalk.bold(`typeorm-seeding v${(pkg as any).version}`))
+    try {
+      const options = await getConnectionOptions(args.config as string)
+      log(options)
+    } catch (error) {
+      printError('Could not find the orm config file', error)
+      process.exit(1)
+    }
+    process.exit(0)
+  }
+}
--- src/connection.ts
+++ src/connection.ts
@@ -2,16 +2,11 @@ import * as path from 'path'
 import {
   Connection,
   createConnection as createTypeORMConnection,
-  getConnectionOptions as getTypeORMConnectionOptions,
   ConnectionOptions,
 } from 'typeorm'
 
 export const getConnectionOptions = async (configPath: string): Promise<ConnectionOptions> => {
-  try {
-    return await getTypeORMConnectionOptions()
-  } catch (_) {
     return require(path.join(process.cwd(), configPath))
-  }
 }
 
 export const createConnection = async (configPath: string): Promise<Connection> => {
