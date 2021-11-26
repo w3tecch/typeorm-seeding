aeeb3767a6ba6a3ecfde57b03c29515814739b20
Jorge Bodega <jorge.bodega.f@gmail.com>
2021-11-26 18:22:23 +0100

    chore: Update yargs and type arguments

--- src/commands/config.command.ts
+++ src/commands/config.command.ts
@@ -1,41 +1,46 @@
-import * as yargs from 'yargs'
+import { Argv, Arguments, CommandModule } from 'yargs'
 import chalk from 'chalk'
 import { printError } from '../utils/log.util'
-import { configureConnection, getConnectionOptions, setConnectionOptions } from '../connection'
-import { getConnectionOptions as TypeORMgetConnectionOptions } from 'typeorm'
+import { configureConnection, getConnectionOptions } from '../connection'
 
-export class ConfigCommand implements yargs.CommandModule {
+interface ConfigCommandArguments extends Arguments {
+  root?: string
+  configName?: string
+  connection?: string
+}
+
+export class ConfigCommand implements CommandModule {
   command = 'config'
   describe = 'Show the TypeORM config'
 
-  builder(args: yargs.Argv) {
+  builder(args: Argv) {
     return args
       .option('n', {
         alias: 'configName',
-        default: '',
+        type: 'string',
         describe: 'Name of the typeorm config file (json or js).',
       })
       .option('c', {
         alias: 'connection',
-        default: '',
+        type: 'string',
         describe: 'Name of the typeorm connection',
       })
       .option('r', {
         alias: 'root',
-        default: process.cwd(),
+        type: 'string',
         describe: 'Path to your typeorm config file',
       })
   }
 
-  async handler(args: yargs.Arguments) {
+  async handler(args: ConfigCommandArguments) {
     const log = console.log
     const { default: pkg } = await import('../../package.json')
     log('🌱  ' + chalk.bold(`TypeORM Seeding v${pkg.version}`))
     try {
       configureConnection({
-        root: args.root as string,
-        configName: args.configName as string,
-        connection: args.connection as string,
+        root: args.root,
+        configName: args.configName,
+        connection: args.connection,
       })
       const options = await getConnectionOptions()
       log(options)
