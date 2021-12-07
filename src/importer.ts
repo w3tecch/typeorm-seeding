dd4a89c1274d9013415db1d45a4130f41069d15d
Jorge Bodega <jorge.bodega.f@gmail.com>
2021-12-07 02:40:22 +0100

    :hammer: refactor: Adapt importer to use single file with default export

--- src/importer.ts
+++ src/importer.ts
@@ -1,8 +1,8 @@
 import { ClassConstructor } from './types'
 
-export const importSeed = async (filePath: string): Promise<ClassConstructor<any>[]> => {
-  const importedElements: { [key: string]: any } = await import(filePath)
-  return Object.values(importedElements).filter(
-    (value) => Object.prototype.toString.call(value) === '[object Function]',
-  )
+export const importSeed = async (filePath: string): Promise<ClassConstructor<any> | undefined> => {
+  const { default: seeder }: { default: ClassConstructor<any> } = await import(filePath)
+
+  if (Object.prototype.toString.call(seeder) === '[object Function]') return seeder
+  return undefined
 }
