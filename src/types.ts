fffe411aa0efcdc90c17409dda29506eb8e6a2aa
Jorge Bodega <jorge.bodega.f@gmail.com>
2021-12-07 02:40:52 +0100

    :hammer: refactor: Change typing

--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,20 @@
 import * as Faker from 'faker'
 import { ObjectType } from 'typeorm'
-
-import { EntityFactory } from './entity-factory'
+import { Factory } from './factory'
 
 /**
- * FactoryFunction is the fucntion, which generate a new filled entity
+ * FactoryFunction is the function, which generate a new filled entity
  */
 export type FactoryFunction<Entity, Context> = (faker: typeof Faker, context?: Context) => Entity
 
 /**
  * Factory gets the EntityFactory to the given Entity and pass the context along
  */
-export type Factory = <Entity, Context>(
+export type ContextFactoryFunction = <Entity, Context>(
   entity: ObjectType<Entity>,
-) => (context?: Context) => EntityFactory<Entity, Context>
+) => (context?: Context) => Factory<Entity, Context>
 
 /**
  * Constructor of the seed class
  */
 export type ClassConstructor<T> = new () => T
-
-/**
- * Value of our EntityFactory state
- */
-export interface EntityFactoryDefinition<Entity, Context> {
-  entity: ObjectType<Entity>
-  factory: FactoryFunction<Entity, Context>
-}
