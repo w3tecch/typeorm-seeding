79b4a59bba98a5be01ab4bff4e27b8960d125738
Jorge Bodega <jorge.bodega.f@gmail.com>
2022-07-01 00:31:27 +0200

    chore: remove unused tests

--- test/utils/fileHandling.test.ts
+++ test/utils/fileHandling.test.ts
@@ -2,7 +2,7 @@ import { calculateFilePath } from '../../src/utils/fileHandling'
 
 describe(calculateFilePath, () => {
   test('Should return a flat array', () => {
-    const results = calculateFilePath(['*.ts'])
+    const results = calculateFilePath('*.ts')
 
     expect(results.length).toBeGreaterThan(0)
     expect(results.some((result) => result.includes('ormconfig.ts'))).toBeTruthy()
